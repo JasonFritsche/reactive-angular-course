f3480b3d6614b09e63a6121575ea8a5af718aed1
jasonfritsche <fritsche.jason@gmail.com>
2023-05-07 14:29:47 -0500

    feat: implement course modification with save button

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
+import { filter, tap } from "rxjs/operators";
 
 @Component({
   selector: "courses-card-list",
@@ -10,6 +11,7 @@ import { CourseDialogComponent } from "../course-dialog/course-dialog.component"
 })
 export class CoursesCardListComponent {
   @Input() courses: Course[] = [];
+  @Output() private courseChanged = new EventEmitter();
 
   constructor(private dialog: MatDialog) {}
 
@@ -23,5 +25,13 @@ export class CoursesCardListComponent {
     dialogConfig.data = course;
 
     const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter((val) => !!val),
+        tap(() => this.courseChanged.emit())
+      )
+      .subscribe();
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,11 +17,13 @@ export class HomeComponent implements OnInit {
   constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
+    this.reloadCourses();
+  }
+
+  reloadCourses() {
     const courses$ = this.coursesService
       .loadAllCourses()
       .pipe(map((courses) => courses.sort(sortCoursesBySeqNo)));
-
-    courses$.subscribe((val) => console.log(val));
 
     this.beginnerCourses$ = courses$.pipe(
       map((course) => course.filter((course) => course.category === "BEGINNER"))
