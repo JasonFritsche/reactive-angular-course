aa15fdb1e567f634c23ef80e73fb1bc652b0c9ab
jasonfritsche <fritsche.jason@gmail.com>
2023-05-17 21:13:12 -0500

    feat: implement eager course save + backend save

--- src/app/services/courses.store.ts
+++ src/app/services/courses.store.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable, of, throwError } from "rxjs";
 import { Course, sortCoursesBySeqNo } from "../model/course";
-import { catchError, map, tap } from "rxjs/operators";
+import { catchError, map, share, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 import { LoadingService } from "../loading/loading.service";
 import { MessagesService } from "../messages/messages.service";
@@ -36,8 +36,33 @@ export class CoursesStore {
     this.loading.showLoaderUntilCompleted(loadCourses$).subscribe();
   }
 
-  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
-    return of();
+  saveCourse(courseId: string, changes: Partial<Course>) {
+    // get the current value of courses subject
+    const courses = this.subject.getValue();
+    // find the index of the course with the specified course id
+    const index = courses.findIndex((course) => course.id === courseId);
+    // update the course with the specified course id, add the changes
+    const updatedCourse: Course = {
+      ...courses[index],
+      ...changes,
+    };
+    // create a copy of the courses array, passing in 0 allows us to copy the entire array
+    const updatedCourses: Course[] = courses.slice(0);
+    // update the course at the specified index
+    updatedCourses[index] = updatedCourse;
+    // emit the updated courses array
+    this.subject.next(updatedCourses);
+
+    // server side save, use shareReplay to prevent multiple http calls
+    return this.http.put(`/api/courses/${courseId}`, changes).pipe(
+      catchError((err) => {
+        const messages = "Could not save course";
+        this.messages.showErrors(messages);
+        console.log(messages, err);
+        return throwError(err);
+      }),
+      shareReplay()
+    );
   }
 
   filterByCategory(category: string): Observable<Course[]> {
