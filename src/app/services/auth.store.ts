8d4298f2e66fbe44d9ca5cc4e71b51045efdd3a4
jasonfritsche <fritsche.jason@gmail.com>
2023-05-21 14:11:08 -0500

    feat: save user profile in localstorage

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,8 @@ import { User } from "../model/user";
 import { map, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
+const AUTH_DATA = "auth_data";
+
 @Injectable({
   providedIn: "root",
 })
@@ -17,16 +19,29 @@ export class AuthStore {
   constructor(private http: HttpClient) {
     this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
     this.isLoggedOut$ = this.isLoggedIn$.pipe(map((loggedIn) => !loggedIn));
+
+    // get user profile from localstorage on app startup
+    const user = localStorage.getItem(AUTH_DATA);
+
+    // if user is in localstorage then publish it
+    if (user) {
+      this.subject.next(JSON.parse(user));
+    }
   }
 
   login(email: string, password: string): Observable<User> {
     return this.http.post<User>("/api/login", { email, password }).pipe(
-      tap((user) => this.subject.next(user)),
+      tap((user) => {
+        this.subject.next(user);
+        localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+      }),
       shareReplay()
     );
   }
 
   logout(): void {
     this.subject.next(null);
+    // remove user from local storage on log out
+    localStorage.removeItem(AUTH_DATA);
   }
 }
